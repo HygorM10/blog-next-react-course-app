b3e140b7a69df3d970de5434c5a8de3463236071
Hygor Martins <hygor.martins74@gmail.com>
2026-03-04 23:16:31 -0300

    feat(error): improve error handling and message display
    * Refactor RouteErrorPage to accept error props
    * Update error message content for better clarity
    * Add 'use client' directive to ErrorMessage component

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,13 +1,23 @@
 'use client';
 
 import ErrorMessage from '@/components/ErrorMessage';
+import { useEffect } from 'react';
 
-export default function RouteErrorPage() {
+type ErrorProps = {
+  error: Error;
+  reset: () => void;
+};
+
+export default function RouteErrorPage({ error }: ErrorProps) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
   return (
     <ErrorMessage
-      pageTitle='Slug'
-      contentTitle='ERROR'
-      content='Ocorreu um erro ao carregar a página. Por favor, tente novamente mais tarde.'
+      pageTitle='Internal Server Error'
+      contentTitle='501'
+      content='Ocorreu um erro inesperado ao qual nossa aplicação não conseguiu lidar. Tente novamente mais tarde.'
     />
   );
 }
